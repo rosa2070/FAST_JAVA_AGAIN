73c3236b6482cdfb8c2c09461a4e77819a2bd444
jaeeunhyun <rosa2070@naver.com>
2021-07-20 01:35:07 +0900

    dfsdfds

--- Chapter6/src/ch21/JoinTest.java
+++ Chapter6/src/ch21/JoinTest.java
@@ -22,6 +22,8 @@ public class JoinTest extends Thread{
 
 	public static void main(String[] args) {
 		
+		System.out.println(Thread.currentThread() + "start");
+		
 
 		JoinTest jt1 = new JoinTest(1, 50);
 		JoinTest jt2 = new JoinTest(51, 100);
@@ -29,12 +31,23 @@ public class JoinTest extends Thread{
 		jt1.start();
 		jt2.start();
 		
+		try {
+			jt1.join();
+			jt2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
 		int lastTotal = jt1.total + jt2.total;
 		
 		System.out.println("jt1.total = " + jt1.total);
 		System.out.println("jt2.total = " + jt2.total);
 		
 		System.out.println("lastTotal = " + lastTotal);
+		System.out.println(Thread.currentThread() + "end");
+
 	}
 
 }
